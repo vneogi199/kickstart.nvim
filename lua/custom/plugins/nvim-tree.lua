38943382db4caa19d837174ac3cd7f647816f809
Vinit Neogi <20491952+vneogi199@users.noreply.github.com>
2023-12-30 01:31:11 +0530

    nvim-tree should open on right

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -1,7 +1,12 @@
 return {
   'nvim-tree/nvim-tree.lua',
   config = function()
-    require("nvim-tree").setup()
+    require("nvim-tree").setup({
+      view = {
+        side = "right",
+        width = 50
+      }
+    })
     vim.keymap.set("n", "<leader>f", ":NvimTreeToggle<cr>", { desc = "Toggle NvimTree" })
   end,
   -- cmd = { "NvimTreeToggle", "NvimTreeOpen", "NvimTreeFocus", "NvimTreeFindFileToggle" },
