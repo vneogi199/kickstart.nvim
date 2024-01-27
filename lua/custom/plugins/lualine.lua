9f88f26de2ca66ba85c9b492410a66a28e2e9376
Vinit Neogi <20491952+vneogi199@users.noreply.github.com>
2024-01-27 19:46:48 +0530

    format lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,21 +1,21 @@
 local function lsp_name(msg)
-  msg = msg or "Inactive"
+  msg = msg or 'Inactive'
   local buf_clients = vim.lsp.get_active_clients()
   if next(buf_clients) == nil then
-    if type(msg) == "boolean" or #msg == 0 then
-      return "Inactive"
+    if type(msg) == 'boolean' or #msg == 0 then
+      return 'Inactive'
     end
     return msg
   end
   local buf_client_names = {}
 
   for _, client in pairs(buf_clients) do
-    if client.name ~= "null-ls" then
+    if client.name ~= 'null-ls' then
       table.insert(buf_client_names, client.name)
     end
   end
 
-  return table.concat(buf_client_names, ", ")
+  return table.concat(buf_client_names, ', ')
 end
 
 return {
@@ -23,22 +23,22 @@ return {
   'nvim-lualine/lualine.nvim',
   -- See `:help lualine.txt`
   dependencies = {
-    'nvim-tree/nvim-web-devicons'
+    'nvim-tree/nvim-web-devicons',
   },
   opts = {
     options = {
       icons_enabled = true,
       theme = 'auto',
-      component_separators = '|',
-      section_separators = '',
+      -- component_separators = '|',
+      -- section_separators = '',
     },
     sections = {
       lualine_y = {
         {
           lsp_name,
-          icon = "",
+          icon = '',
         },
       },
-    }
+    },
   },
 }
