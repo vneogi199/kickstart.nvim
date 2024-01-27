d0045159cde561faa04045d6d997d8983fd36f2b
Vinit Neogi <20491952+vneogi199@users.noreply.github.com>
2024-01-27 19:47:37 +0530

    disable nvim-tree

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -1,14 +1,14 @@
 return {
-  'nvim-tree/nvim-tree.lua',
-  config = function()
-    require("nvim-tree").setup({
-      view = {
-        side = "right",
-        width = 50
-      }
-    })
-    vim.keymap.set("n", "<leader>f", ":NvimTreeToggle<cr>", { desc = "Toggle NvimTree" })
-  end,
+  -- 'nvim-tree/nvim-tree.lua',
+  -- config = function()
+  --   require("nvim-tree").setup({
+  --     view = {
+  --       side = "right",
+  --       width = 50
+  --     }
+  --   })
+  --   vim.keymap.set("n", "<leader>f", ":NvimTreeToggle<cr>", { desc = "Toggle NvimTree" })
+  -- end,
   -- cmd = { "NvimTreeToggle", "NvimTreeOpen", "NvimTreeFocus", "NvimTreeFindFileToggle" },
   -- event = "User DirOpened",
 }
