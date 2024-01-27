b9676d9ed7555fa83f3c154785bb1fd7863215b7
Vinit Neogi <20491952+vneogi199@users.noreply.github.com>
2024-01-27 19:47:24 +0530

    disable linting with nvim-lint

--- lua/custom/plugins/nvim-lint.lua
+++ lua/custom/plugins/nvim-lint.lua
@@ -5,26 +5,26 @@ return {
     'BufNewFile',
   },
   config = function()
-    local lint = require('lint');
+    local lint = require 'lint'
     lint.linters_by_ft = {
-      javascript = { "eslint_d" },
-      typescript = { "eslint_d" },
-      javascriptreact = { "eslint_d" },
-      typescriptreact = { "eslint_d" },
+      -- javascript = { "eslint_d" },
+      -- typescript = { "eslint_d" },
+      -- javascriptreact = { "eslint_d" },
+      -- typescriptrect = { "eslint_d" },
       -- svelte = { "eslint_d" },
       -- python = { "pylint" },
     }
-    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+    local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
 
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
       group = lint_augroup,
       callback = function()
         lint.try_lint()
       end,
     })
 
-    vim.keymap.set("n", "<leader>l", function()
+    vim.keymap.set('n', '<leader>l', function()
       lint.try_lint()
-    end, { desc = "Trigger linting for current file" })
-  end
+    end, { desc = 'Trigger linting for current file' })
+  end,
 }
