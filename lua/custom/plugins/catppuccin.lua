8bb28ff42a7b921fd376f318e6bb8a475df2182e
Vinit Neogi <20491952+vneogi199@users.noreply.github.com>
2024-03-16 16:34:04 +0530

    change to catpuccin mocha

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -3,7 +3,8 @@ return {
   name = 'catppuccin',
   lazy = false,
   config = function()
-    vim.cmd.colorscheme 'catppuccin-frappe'
+    -- vim.cmd.colorscheme 'catppuccin-frappe'
+    vim.cmd.colorscheme 'catppuccin'
   end,
   priority = 1000,
 }
