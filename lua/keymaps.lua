2899464f3f1e915568ffe1a3b5ef63fc13ae1127
Vinit Neogi <20491952+vneogi199@users.noreply.github.com>
2024-03-24 00:49:08 +0530

    add keymap for macro replay

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -73,6 +73,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', 'Q', '@qj')
+vim.keymap.set('x', 'Q', ':norm @q<CR>')
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
