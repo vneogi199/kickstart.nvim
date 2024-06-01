15f76fafc46bbc968c45937220e7ebb2e3806c95
Vinit Neogi <20491952+vneogi199@users.noreply.github.com>
2024-06-01 13:47:26 +0530

    add tailwindcss lsp

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -166,7 +166,7 @@ return {
         tsserver = {},
         eslint = {},
         html = { filetypes = { 'html', 'twig', 'hbs' } },
-
+        tailwindcss = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
